186180b13fa7aa5bd0c3cce4d28bc814dab3174e
Ian TAKARA <e245718@ie.u-ryukyu.ac.jp>
2024-12-16 16:53:32 +0900

    1st commit

--- src/Report5.java
+++ src/Report5.java
@@ -1,12 +1,6 @@
 public class Report5 {
     public static void main(String[] args) {
         String str = null;
-        
-        try {
-            int length = str.length();
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException Was recieved");
-            System.out.println("例外メッセージ: " + e.getMessage());
-        }
+        System.out.println(str.length());
     }
 }
